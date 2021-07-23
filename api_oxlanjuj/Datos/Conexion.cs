c80629abb6ecafebbb9f5386d05ce612bfb0489a
Osiel Esquivel Pinto <osielep@gmail.com>
2021-07-22 21:08:50 -0600

    Cadena de conexión

---
+++ api_oxlanjuj/Datos/Conexion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    class Conexion
+    {
+        private static string Usuario   = "sa";
+        private static string Password  = "1234";
+        private static string Server    = "MSI\\SQLEXPRESS";
+        private static string DB        = "OXLANJUJ";
+
+        private static string CadenaConexionSQL()
+        {
+            return  "Persist Security info = False; User ID = '" + Usuario +
+                    "'; Passowrd = '" + Password +
+                    "'; Initial Catalog = '" + DB +
+                    "'; Server = '" + Server + "'";
+        }
+    }
+}

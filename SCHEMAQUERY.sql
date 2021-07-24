fc3b768a04bb25945f78735d1e87c5584b6eab27
Osiel Esquivel Pinto <osielep@gmail.com>
2021-07-23 22:27:48 -0600

    Create SCHEMAQUERY.sql

---
+++ SCHEMAQUERY.sql
@@ -0,0 +1,16 @@
+ALTER SCHEMA Sesion TRANSFER dbo.TblToken
+ALTER SCHEMA Sesion TRANSFER dbo.TblUsuario
+ALTER SCHEMA Sesion TRANSFER dbo.TblRol
+ALTER SCHEMA Sesion TRANSFER dbo.TblInstitucion
+
+ALTER SCHEMA Idiomas TRANSFER Aula.TblAutor
+ALTER SCHEMA Idiomas TRANSFER Aula.TblPalabra
+ALTER SCHEMA Idiomas TRANSFER Aula.TblIdioma
+ALTER SCHEMA Idiomas TRANSFER Aula.TblTipoDePalabra
+ALTER SCHEMA Idiomas TRANSFER Aula.TblCtaGramatical
+
+ALTER SCHEMA Aula TRANSFER dbo.TblTipoDeEvaluacion
+ALTER SCHEMA Aula TRANSFER dbo.TblWorksheetHeader
+ALTER SCHEMA Aula TRANSFER dbo.TblWorksheetBody
+ALTER SCHEMA Aula TRANSFER dbo.TblWorksheetSeccion
+ALTER SCHEMA Aula TRANSFER dbo.TblWorksheetPalabra

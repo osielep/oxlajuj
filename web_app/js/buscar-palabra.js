abf9a5d48e3bbe7fa4ed9635fe8de25ee275c4b3
Osiel Esquivel Pinto <osielep@gmail.com>
2021-08-02 18:21:49 -0600

    traductor v0.1

--- web_app/js/buscar-palabra.js
+++ web_app/js/buscar-palabra.js
@@ -1,4 +1,5 @@
 var urlApi = "http://localhost:60957/api/"
+var LimpiarDiv = "";
 
 function BuscarPalabra() {
     var settings = {
@@ -19,11 +20,53 @@ function BuscarPalabra() {
             var MejorResultado = data.TxtPalabraIdiomaMaya;
             document.getElementById('TxtMejorResultado').innerHTML = MejorResultado;
             Limpiar()
+
         });
+
+        //OracionesDeEjemplo()
     });
 }
 
 function Limpiar() {
+
     $("#TxtPalabraTraductor").val("");
+
+    //$("#ResultadoBusquedaEjemplos").val("");
+    //$(ResultadoBusquedaEjemplos).prepend(" ");
+
 }
 var palabra = 'pasdas \' \' '
+
+
+function OracionesDeEjemplo() {
+    var settings = {
+        "url": urlApi + "BuscarOracionDeEjemplo",
+        "method": "POST",
+        "timeout": 0,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "data": JSON.stringify({
+            "TxtPalabraEspanol": $("#TxtPalabraTraductor").val()
+        }),
+    };
+
+    document.getElementById('ResultadoBusquedaEjemplos').innerHTML = LimpiarDiv;
+
+    $.ajax(settings).done(function(response) {
+
+        $.each(
+            response,
+            function(index, data) {
+
+                var idDiv = data.TxtPalabraEspanol;
+                var OracionEspanol = data.TxOracionEspanol;
+                var OracionMaya = data.TxtOracionIdiomaMaya;
+
+                $(ResultadoBusquedaEjemplos).prepend("<div id=" + idDiv + "> <small><em>Español:</em></small> " + OracionEspanol + " <br> <small><em>Q'eqchi':</em></small> " + OracionMaya + " </div> <br>");
+
+                console.log(response);
+            }
+        );
+    });
+}

87c1d17f9096b49ed57029fd6be691fe7b347a85
Osiel Esquivel Pinto <osielep@gmail.com>
2021-08-18 22:08:23 -0600

    traductor v0.2

--- web_app/js/buscar-palabra.js
+++ web_app/js/buscar-palabra.js
@@ -1,6 +1,17 @@
 var urlApi = "http://localhost:60957/api/"
 var LimpiarDiv = "";
 
+var CardBodyEjemplos = '<dl class="row">'
+'<dt class="col-sm-2">' +
+'<small class="text-warning">qeqchi<em></em></small>' +
+'<small class="text-success "><em>español</em></small>' +
+'</dt>' +
+'<dd class="col-sm-10">' +
+'<span>Xinxk’at li amche’.</span><br>' +
+'<span>El palo brujo me quemó.</span>' +
+'</dd>' +
+'</dl>';
+
 function BuscarPalabra() {
     var settings = {
         "url": urlApi + "BuscarPalabra",
@@ -10,7 +21,7 @@ function BuscarPalabra() {
             "Content-Type": "application/json"
         },
         "data": JSON.stringify({
-            "TxtPalabraEspanol": $("#TxtPalabraTraductor").val()
+            "TxtPalabraEspanol": $("#PalabraBuscar").val()
         }),
     };
 
@@ -18,8 +29,22 @@ function BuscarPalabra() {
         console.log(response);
         $.each(response, function(index, data) {
             var MejorResultado = data.TxtPalabraIdiomaMaya;
+            var CategoriaGramatical = data.TxtNombreCtaGramatical;
+            var TipoPalabra = data.TxtNombreTipoPalabra;
+            var PalbraEspanolFrt = $("#PalabraBuscar").val();
+
+            var valor = '';
+            document.getElementById('DefPlEsp').innerHTML = valor;
+            document.getElementById('CtaGramFrt').innerHTML = valor;
+            document.getElementById('TipoPalrabFrt').innerHTML = valor;
+
             document.getElementById('TxtMejorResultado').innerHTML = MejorResultado;
-            Limpiar()
+            $(DefPlEsp).prepend(PalbraEspanolFrt);
+            $(CtaGramFrt).prepend(CategoriaGramatical);
+            $(TipoPalrabFrt).prepend(TipoPalabra);
+
+            console.log(PalbraEspanolFrt)
+                //Limpiar()
 
         });
 
@@ -27,9 +52,13 @@ function BuscarPalabra() {
     });
 }
 
-function Limpiar() {
+function LimpiarSpan() {
 
-    $("#TxtPalabraTraductor").val("");
+    var valor = '';
+    document.getElementById('DefPlEsp').innerHTML = valor;
+    document.getElementById('CtaGramFrt').innerHTML = valor;
+    document.getElementById('TipoPalrabFrt').innerHTML = valor;
+
 
     //$("#ResultadoBusquedaEjemplos").val("");
     //$(ResultadoBusquedaEjemplos).prepend(" ");
@@ -47,11 +76,11 @@ function OracionesDeEjemplo() {
             "Content-Type": "application/json"
         },
         "data": JSON.stringify({
-            "TxtPalabraEspanol": $("#TxtPalabraTraductor").val()
+            "TxtPalabraEspanol": $("#PalabraBuscar").val()
         }),
     };
 
-    document.getElementById('ResultadoBusquedaEjemplos').innerHTML = LimpiarDiv;
+    //document.getElementById('ResultadoBusquedaEjemplos').innerHTML = LimpiarDiv;
 
     $.ajax(settings).done(function(response) {
 
@@ -62,8 +91,13 @@ function OracionesDeEjemplo() {
                 var idDiv = data.TxtPalabraEspanol;
                 var OracionEspanol = data.TxOracionEspanol;
                 var OracionMaya = data.TxtOracionIdiomaMaya;
+                var oraciones1234 = "<div id=" + idDiv + "> <small class='text-warning'><em>Q'eqchi'</em></small> " + OracionMaya + " <br> <span class='small text-muted'><em class='text-success'>Espanol </em>" + OracionEspanol + "</span> </div> <br>";
+                var PalbraEspanolFrt = $("#PalabraBuscar").val();
+                var valor = '';
+                document.getElementById('ComoUsarFrt').innerHTML = valor;
 
-                $(ResultadoBusquedaEjemplos).prepend("<div id=" + idDiv + "> <small><em>Español:</em></small> " + OracionEspanol + " <br> <small><em>Q'eqchi':</em></small> " + OracionMaya + " </div> <br>");
+                $(ComoUsarFrt).prepend(PalbraEspanolFrt);
+                $(deftraductor).prepend(oraciones1234);
 
                 console.log(response);
             }

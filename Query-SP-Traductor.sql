1e9aeb0de1faf72c29327cbde0615cfeddfbc947
Osiel Esquivel Pinto <osielep@gmail.com>
2021-08-21 16:45:51 -0600

    logo

--- Query-SP-Traductor.sql
+++ Query-SP-Traductor.sql
@@ -9,37 +9,57 @@
 	|  Modificado: 05/08/2021				|
 	|  Detalle:	Se agrego autor				|
 	+---------------------------------------+
+	|  Modificado: 18/08/2021				|
+	|  Detalle:	Se agrego acomulativo		|
+	+---------------------------------------+
 */
 
 alter PROC Idiomas.SPBuscarPalabra (@TxtPalabraEspanol NVARCHAR(50))
 AS
+DECLARE @IdPalabraAcumulativo INT
 BEGIN
 	
+	SELECT @IdPalabraAcumulativo = ISNULL(MAX(p.IdPalabra),0)
+	FROM 
+		Idiomas.TblAutor a,
+		Idiomas.TblCtaGramatical g,
+		Idiomas.TblIdioma i,
+		Idiomas.TblPalabra p,
+		Idiomas.TblTipoDePalabra t
+	WHERE 
+		p.IdAutor = a.IdAutor and
+		p.IdCategoriaGramatical = g.IdCtaGramatical and
+		p.IdIdioma = i.IdIdioma and
+		p.IdTipoPalabra = t.IdTipoPalabra and
+		p.TxtPalabraEspanol LIKE '%'+@TxtPalabraEspanol+'%';
+	----------------------------------------------------------------
 	SELECT
+		p.IdPalabra,
+		p.TxtPalabraIdiomaMaya,
+		p.TxtGenero,
+		p.TxtImg,
+		p.TxtAudio,
+		t.TxtNombreTipoPalabra,
+		g.TxtNombreCtaGramatical,
+		CONCAT (a.TxtNombreAutor, ' ', a.TxtApellidoAutor) as TxtAutor
+
+
+	FROM
+		Idiomas.TblAutor a,
+		Idiomas.TblCtaGramatical g,
+		Idiomas.TblIdioma i,
+		Idiomas.TblPalabra p,
+		Idiomas.TblTipoDePalabra t
+
+	WHERE	
 	
-	p.TxtPalabraIdiomaMaya,
-	p.TxtGenero,
-	p.TxtImg,
-	p.TxtAudio,
-	t.TxtNombreTipoPalabra,
-	g.TxtNombreCtaGramatical,
-	CONCAT (a.TxtNombreAutor, ' ', a.TxtApellidoAutor) as TxtAutor
+		p.IdAutor = a.IdAutor and
+		p.IdCategoriaGramatical = g.IdCtaGramatical and
+		p.IdIdioma = i.IdIdioma and
+		p.IdTipoPalabra = t.IdTipoPalabra and
+		p.TxtPalabraEspanol LIKE '%'+@TxtPalabraEspanol+'%'
 
-
-FROM
-	Idiomas.TblAutor a,
-	Idiomas.TblCtaGramatical g,
-	Idiomas.TblIdioma i,
-	Idiomas.TblPalabra p,
-	Idiomas.TblTipoDePalabra t
-
-WHERE	
-	
-	p.IdAutor = a.IdAutor and
-	p.IdCategoriaGramatical = g.IdCtaGramatical and
-	p.IdIdioma = i.IdIdioma and
-	p.IdTipoPalabra = t.IdTipoPalabra and
-	p.TxtPalabraEspanol LIKE '%'+@TxtPalabraEspanol+'%'
+	EXEC Idiomas.SPAgregarAcumulado @IdPalabraAcumulativo
 
 END
 
@@ -90,3 +110,46 @@ END
 exec Idiomas.SPBuscarOracionesDeEjemplo taltuza
 
 CONCAT (o.TxOracionEspanol, ' // ', o.TxtOracionIdiomaMaya) as TxtOracionEjemplo
+
+
+
+
+
+/*
+	+---------------------------------------+
+	|	Tipo:	Agregar acumulado			|
+	+---------------------------------------+
+	|	Autor:	Widman Esquivel				|
+	|	Fecha:	18/08/2021					|
+	+---------------------------------------+
+*/
+
+CREATE PROC Idiomas.SPAgregarAcumulado (@IdPalabra INT)
+AS
+DECLARE @FilasAfectadas TINYINT,
+		@Resultado		INT
+BEGIN
+	BEGIN TRAN
+
+		BEGIN TRY
+			update idiomas.TblPalabra
+			set AcumuladoBusquedas = AcumuladoBusquedas + 1
+			where IdPalabra = @IdPalabra
+			SET @FilasAfectadas						= @@ROWCOUNT
+
+		END TRY
+
+		BEGIN CATCH
+			SET @FilasAfectadas						= 0
+		END CATCH
+
+		-- Comprobación
+		IF(@FilasAfectadas > 0)
+			BEGIN
+				COMMIT
+			END
+		ELSE
+			BEGIN
+				ROLLBACK 
+			END
+END 
